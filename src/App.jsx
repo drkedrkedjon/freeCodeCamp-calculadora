644c784b1a6e4e08ef0756d7e3e3c3fbe10e29c8
Sasa Savic Perusina <sasa@bubulazy.com>
2023-05-17 20:52:12 +0100

    Starting again, shit happens

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,54 @@ function App() {
   const [displayNum, setDisplayNum] = useState("0");
   const [operandi, setOperandi] = useState(null);
   const [firstNum, setFirstNum] = useState(null);
+  const [newDisplay, setNewdisplay] = useState(false);
 
   function handleNumero(numero) {
-    // No permitir múltiples ceros consecutivos al inicio
+    // Primero debe averiguar si display numeber es 0 y si mandamos 0 en tal caso no hacer nada
+    // Si empezamos con 0 el numero que ponemos (4) debe ser puesto en display en lugar de 0 a no ser que se trata de decimal .
+    // Si es decimal debe concatenarse
+    // Si hay un decimal no debe poner el otro
+    // Si es cualquer otro numero debe concatenarse
+    // Si hay numero guardado sustituir el de display igual como si esta 0
+
     if (displayNum === "0" && numero === "0") {
-      console.log("Numero consecutivo");
+      console.log("Zero consecutivo");
       return;
     }
-    if (displayNum === "0" || operandi !== null) {
-      console.log("display 0 o no hay operandi");
+    const isThereDecimal = displayNum.includes(".");
+
+    if (displayNum === "0" && numero === "." && !isThereDecimal) {
+      console.log("Punto concatenado");
+      setDisplayNum(displayNum + numero);
+    } else if (displayNum === "0" || newDisplay) {
+      console.log("Numero 0 sustituido");
       setDisplayNum(numero);
-      setOperandi(null);
-      // Si el displayNum es 0 o hay operator en state, asignar el valor del numero sin concatenar y borrar operador
+      // setNewdisplay(false);
+    } else if (numero === ".") {
+      if (!isThereDecimal) {
+        console.log("Added . en un numero normal");
+        setDisplayNum(displayNum + numero);
+      }
     } else {
-      console.log("De mas numeros");
+      console.log("numero concatenado");
       setDisplayNum(displayNum + numero);
     }
-  }
-  function handleOperandi(operator) {
-    console.log(operator);
-    // Si el primer numero es null, asignar el de display como el primer numero
-    if (firstNum === null) {
-      setFirstNum(parseFloat(displayNum));
-    } else {
-      console.log("cualquier operandi");
-      // Realizar el cálculo si ya hay un operador y no es "="
-      const result = calcularResultado(operator);
-      // Asignar ambos, display y first numero el resultado de calculo
-      setDisplayNum(result);
-      setFirstNum(result);
-    }
-    setOperandi(operator);
+
+    // // No permitir múltiples ceros consecutivos al inicio
+    // if (displayNum === "0" && numero === "0") {
+    //   console.log("Numero consecutivo");
+    //   return;
+    // }
+    // if (displayNum === "0" || control === false) {
+    //   console.log("display 0 o hay operandi");
+    //   setDisplayNum(numero);
+    //   setControl(true);
+    //   // Si el displayNum es 0 o hay operator en state, asignar el valor del numero sin concatenar y borrar operador
+    // } else {
+    //   console.log("De mas numeros");
+
+    //   setDisplayNum(displayNum + numero);
+    // }
   }
   function calcularResultado(operator) {
     // Convertir display numero string en numero numero y guardarlo
@@ -46,7 +63,7 @@ function App() {
         return firstNum + secondNum;
       case "-":
         return firstNum - secondNum;
-      case "*":
+      case "x":
         return firstNum * secondNum;
       case "/":
         return firstNum / secondNum;
@@ -54,6 +71,26 @@ function App() {
         return secondNum;
     }
   }
+
+  function handleOperandi(operator) {
+    // Si el primer numero es null, asignar el de display como el primer numero
+    if (firstNum === null) {
+      console.log("cargar primer numero");
+      setFirstNum(parseFloat(displayNum));
+      setOperandi(operator);
+      setNewdisplay(true);
+    } else {
+      console.log("cualquier operandi");
+      // Realizar el cálculo si ya hay un operador y no es "="
+      const result = calcularResultado(operator);
+      // Asignar ambos, display y first numero el resultado de calculo
+      setDisplayNum(result);
+      setFirstNum(result);
+      setNewdisplay(true);
+    }
+    setOperandi(operator);
+  }
+  console.log(operandi);
   function handleAC() {
     // Resetear los tres estado
     setDisplayNum("0");
@@ -90,7 +127,7 @@ function App() {
       <Button
         type="operandi"
         handleClick={handleOperandi}
-        value="*"
+        value="x"
         id="multiply"
         color="grey"
       />
